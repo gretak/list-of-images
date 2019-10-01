662d6cce5fd48bab82215252129982b81f8df509
Greta Kavaliauskaite <kavaliauskaite3@gmail.com>
2019-10-01 22:42:19 +0100

    fixed vue path issue in dist

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,16 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
+    baseURL: "https://jsonplaceholder.typicode.com",
     withCredentials: false,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
-})
+});
 
 export default {
     getImages(perPage, page) {
         return apiClient.get('/photos?_limit=' + perPage + '&_page=' + page)
     }
-}
+};
